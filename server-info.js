beb4e5581ac3f0bff55f96e5e064faade3ec033d
lumix103 <15038234+lumix103@users.noreply.github.com>
2019-10-16 23:14:05 -0700

    Delete server-info.js

--- server-info.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'server-info',
-    guildOnly:true,
-	description: 'Shows public server info.',
-	execute(msg, args) {
-		msg.reply(`Soupless was created in ${msg.guild.createdAt}`);
-	},
-};
