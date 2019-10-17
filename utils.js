9546dbd9a43515c4846421ef603624ce4abecf88
BuildTools <unconfigured@null.spigotmc.org>
2019-10-16 22:58:11 -0700

    Updated Utils.js

--- utils.js
+++ utils.js
@@ -11,5 +11,42 @@ module.exports =
             return null;
         const id = mentions[1];
         return client.users.get(id);
+    },
+    async parseMentionsToTags(client,str)
+    {
+        if(!client || !str)
+        {
+            return null;
+        }
+        const mentions = str.match(/<@!?(\d+)>/g);
+        if(mentions.length <= 0)
+            return null;
+        let cpyStr = str;
+        for(let i = 0; i < mentions.length; i++)
+        {
+            const user = await client.fetchUser(mentions[i].match(/(\d+)/g)).then(v => 
+                {
+                    cpyStr = cpyStr.replace(mentions[i],v.tag);
+                    console.log("forEach"+cpyStr);
+                }).catch(err =>
+                    {
+                        console.log(err);
+                    });
+        }
+        console.log(mentions);
+        return cpyStr;
+    }, 
+    makeDefaultEmbed(msg)
+    {
+        if(!msg)
+            return null;
+        let embedColor = parseInt(bot_color);
+        if(isNaN(embedColor))
+            embedColor = 0xff9e49;
+        let embedReply = new Discord.RichEmbed()
+            .setColor(embedColor)
+            .setThumbnail(msg.client.user.displayAvatarURL)
+            .setTimestamp()
+            .setFooter(`~3~`, msg.client.user.displayAvatarURL);
     } 
 }
